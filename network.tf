b3c2232a4a624a7099e9aabfc377dd5058e29743
bindu987654321 <93935226+bindu987654321@users.noreply.github.com>
2024-09-09 15:39:36 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,20 +1,20 @@
 
 resource "azurerm_virtual_network" "demovirtualnetwork" {
-  name                = "terra-demo-vnet"
+  name                = "terra-demo-vnet-1"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.demoresourcegroup.location
   resource_group_name = azurerm_resource_group.demoresourcegroup.name
 }
 
 resource "azurerm_subnet" "demosubnet" {
-  name                 = "subnetabc"
+  name                 = "subnetabc-1"
   resource_group_name  = azurerm_resource_group.demoresourcegroup.name
   virtual_network_name = azurerm_virtual_network.demovirtualnetwork.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_public_ip" "demopublicip" {
-  name                = "demolinuxmachine-pip"
+  name                = "demolinuxmachine-pip-1"
   resource_group_name = azurerm_resource_group.demoresourcegroup.name
   location            = azurerm_resource_group.demoresourcegroup.location
   allocation_method   = "Static"
